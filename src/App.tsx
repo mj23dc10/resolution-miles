b10692d5eb6173b37cb44b8ad5ba9b50d4f070e0
dc <dc>
2021-01-24 15:54:55 -0700

    update for push/pull-ups

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,21 @@ function App() {
   const [milesToAdd, setMilesToAdd] = useState(0);
   const [totalMiles, setTotalMiles] = useState(0);
 
+  const [pushupsToAdd, setPushupsToAdd] = useState(0);
+  const [totalPushups, setTotalPushups] = useState(0);
+
+  const [pullupsToAdd, setPullupsToAdd] = useState(0);
+  const [totalPullups, setTotalPullups] = useState(0);
+
   useEffect(() => {
     const totalMiles = localStorage.getItem('totalMiles');
     totalMiles === null ? setTotalMiles(0) : setTotalMiles(parseFloat(totalMiles));
+
+    const totalPushups = localStorage.getItem('totalPushups');
+    totalPushups === null ? setTotalPushups(0) : setTotalPushups(parseFloat(totalPushups));
+
+    const totalPullups = localStorage.getItem('totalPullups');
+    totalPullups === null ? setTotalPullups(0) : setTotalPullups(parseFloat(totalPullups));
   }, []);
 
   function isLeapYear() {
@@ -30,13 +42,13 @@ function App() {
     return result;
   }
 
-  function milesYouShouldHaveByToday() {
+  function numberYouShouldHaveByToday(goal: number) {
     var daysThisYear = 365;
     if (isLeapYear()) {
       daysThisYear = 366;
     }
-    var milesPerDay = 500 / daysThisYear;
-    var result = daysIntoYear() * milesPerDay;
+    var numberPerDay = goal / daysThisYear;
+    var result = daysIntoYear() * numberPerDay;
     return result.toFixed(2);
   }
 
@@ -50,20 +62,64 @@ function App() {
     localStorage.setItem('totalMiles', newMiles.toString());
   }
 
+  const addPushupsChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setPushupsToAdd(parseFloat(event.currentTarget.value));
+  }
+
+  const addPushupsHandler = () => {
+    var newPushups = totalPushups + pushupsToAdd;
+    setTotalPushups(newPushups);
+    localStorage.setItem('totalPushups', newPushups.toString());
+  }
+
+  const addPullupsChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setPullupsToAdd(parseFloat(event.currentTarget.value));
+  }
+
+  const addPullupsHandler = () => {
+    var newPullups = totalPullups + pullupsToAdd;
+    setTotalPullups(newPullups);
+    localStorage.setItem('totalPullups', newPullups.toString());
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <div className="Row">
-          <text className="statuslabel">500 mile:</text>
-          <text className="status"> {totalMiles} of {milesYouShouldHaveByToday()}---</text>
-          <form className="inputsubmit" onSubmit={addMilesHandler}>
-            <label className="statuslabel">
-              <input className="input" type="text" name="miles" onChange={addMilesChangeHandler} />
-            </label>
-            <input className="submit" type="submit" value="Add" />
-          </form>
-        </div>
-      </header>
+      <div className="Row">
+        <text className="statuslabel">500 mile:</text>
+        <text className="status"> {totalMiles} of {numberYouShouldHaveByToday(500)}</text>
+      </div>
+      <div className="Row">
+        <form className="inputsubmit" onSubmit={addMilesHandler}>
+          <label className="statuslabel">
+            <input className="input" type="text" name="miles" onChange={addMilesChangeHandler} />
+          </label>
+          <input className="submit" type="submit" value="Add" />
+        </form>
+      </div>
+      <div className="Row">
+        <text className="statuslabel">30,000 Push-Ups:</text>
+        <text className="status"> {totalPushups} of {numberYouShouldHaveByToday(30000)}</text>
+      </div>
+      <div className="Row">
+        <form className="inputsubmit" onSubmit={addPushupsHandler}>
+          <label className="statuslabel">
+            <input className="input" type="text" name="pushups" onChange={addPushupsChangeHandler} />
+          </label>
+          <input className="submit" type="submit" value="Add" />
+        </form>
+      </div>
+      <div className="Row">
+        <text className="statuslabel">3,000 Pull-Ups:</text>
+        <text className="status"> {totalPullups} of {numberYouShouldHaveByToday(3000)}</text>
+      </div>
+      <div className="Row">
+        <form className="inputsubmit" onSubmit={addPullupsHandler}>
+          <label className="statuslabel">
+            <input className="input" type="text" name="pullups" onChange={addPullupsChangeHandler} />
+          </label>
+          <input className="submit" type="submit" value="Add" />
+        </form>
+      </div>
     </div>
   );
 }
---
+++ src/TrackerProp.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+interface TrackerProp {
+    goal: number;
+    current: number;
+    
+}
